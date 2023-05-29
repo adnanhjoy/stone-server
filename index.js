72e7a4d1ca2987d1ea62b4b36e5edb3d468bc18f
engadnanhossain <engr.adnanhossain@gmail.com>
2023-05-29 12:18:37 +0600

    added products

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 5000;
 
 const categories = require('./data/category.json');
 const stones = require('./data/stone.json');
-const slabs = require('./data/slabs.json')
+const slabs = require('./data/slabs.json');
+const marbles = require('./data/marble.json');
+const kitchens = require('./data/kitchen.json');
+const storages = require('./data/storage.json');
+const fireplaces = require('./data/firplace.json');
+const floorings = require('./data/flooring.json');
+const bathrooms = require('./data/bathroom.json');
+const granites = require('./data/granite.json');
 
 app.use(cors());
 
@@ -16,8 +23,37 @@ app.get('/categories', (req, res) => {
 app.get('/stones', (req, res) => {
     res.send(stones)
 })
+
 app.get('/slabs', (req, res) => {
     res.send(slabs)
+})
+
+app.get('/marbles', (req, res) => {
+    res.send(marbles)
+})
+
+app.get('/kitchens', (req, res) => {
+    res.send(kitchens)
+})
+
+app.get('/storages', (req, res) => {
+    res.send(storages)
+})
+
+app.get('/fireplaces', (req, res) => {
+    res.send(fireplaces)
+})
+
+app.get('/floorings', (req, res) => {
+    res.send(floorings)
+})
+
+app.get('/granites', (req, res) => {
+    res.send(granites)
+})
+
+app.get('/bathrooms', (req, res) => {
+    res.send(bathrooms)
 })
 
 app.get('/', (req, res) => {
